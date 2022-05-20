8f689764107cb1110374cd524e7fc28ac5e998ad
StienBoodts <stien.boodts@gmail.com>
2022-05-20 07:14:01 +0000

    add javascript function to show info on marker click

--- assets/scripts/js/script.js
+++ assets/scripts/js/script.js
@@ -1,10 +1,14 @@
-// Showing map and home pin - Google tutorial
+
+// coordinates for activities
+
+const horizon = { lat: 51.25463217634249, lng: 2.9819950190502347};
+const seaLife = { lat: 51.31815377413242, lng: 3.145796440478582};
+const horizonEducatief = { lat: 51.23966828124294, lng: 2.9385712521145586};
+const hangTime = { lat:51.23817291526946, lng: 2.939153238042269};
+
+// Showing map and pins - Google tutorial
 
 function initMap() {
-    const horizon = { lat: 51.25463217634249, lng: 2.9819950190502347};
-    const seaLife = { lat: 51.31815377413242, lng: 3.145796440478582};
-    const horizonEducatief = { lat: 51.23966828124294, lng: 2.9385712521145586};
-    const hangTime = { lat:51.23817291526946, lng: 2.939153238042269};
 
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 11,
@@ -32,9 +36,34 @@ function initMap() {
         map: map,
     });
 
+    horizonMarker.addListener("click", () => {
+    document.getElementById("activity-info").innerHTML = `
+    <h3>You are staying here!</h3>`;
+    });
+
+    seaLifeMarker.addListener("click", () => {
+      document.getElementById("activity-info").innerHTML = `
+      <h3>Sea Life</h3>
+      <p>Discover the magic of the underwater world.
+      Come face to face with wildlife and learn all about how we can help ocean conservation.<br>
+      Opening Hours: 10:00 - 17:00<br>
+      Price: €15/person</p>`;
+      });
+
 
   }
   
-  window.initMap = initMap;
+window.initMap = initMap;
+
+let activityInfo = getElementById("activity-info").innerHTML;
+
+let addToItinerary = getElementById("addToItinerary");
+
+let itinerary = [];
+
+addToItinerary.addListener('click', function() {
+  itinerary.push(activityInfo);
+})
+
 
 
