fd5874f10f8fd2caa5602b77e67e9b08a2910946
StienBoodts <stien.boodts@gmail.com>
2022-05-22 21:17:52 +0000

    improve javascript code with for loop for future ease of adding content

--- assets/scripts/js/script.js
+++ assets/scripts/js/script.js
@@ -1,69 +1,110 @@
 
-// coordinates for activities
-
-const horizon = { lat: 51.25463217634249, lng: 2.9819950190502347};
-const seaLife = { lat: 51.31815377413242, lng: 3.145796440478582};
-const horizonEducatief = { lat: 51.23966828124294, lng: 2.9385712521145586};
-const hangTime = { lat:51.23817291526946, lng: 2.939153238042269};
-
 // Showing map and pins - Google tutorial
+let map;
 
 function initMap() {
 
-    const map = new google.maps.Map(document.getElementById("map"), {
+    // coordinates for Horizon
+
+    const horizon = { lat: 51.25463217634249, lng: 2.9819950190502347};
+
+    // insert map centered on Horizon
+    
+    map = new google.maps.Map(document.getElementById("map"), {
       zoom: 11,
       center: horizon,
     });
+
+    // activity coordinates and info
+
+    const activities = [
+      {
+        position: new google.maps.LatLng(51.31815377413242, 3.145796440478582),
+        name: "seaLife",
+        info: `
+        <h3>Sea Life</h3>
+        <p>Discover the magic of the underwater world.
+        Come face to face with wildlife and learn all about how we can help ocean conservation.<br>
+        Opening Hours: 10:00 - 17:00<br>
+        Price: €15/person</p>`,
+      },
+      {
+        position: new google.maps.LatLng(51.23966828124294, 2.9385712521145586),
+        name: "horizonEducatief",
+        info: `
+        <h3>Horizon Educatief</h3>
+        <p>Choose from a wide range of educational and artistic guided activities.<br>
+        From exploring the dunes, learning all about plankton to meeting Pirate Pete.<br>
+        Opening Hours: 10:00 - 17:00<br>
+        Price: Depends on activity.</p>`,
+      },
+      {
+        position: new google.maps.LatLng(51.23817291526946, 2.939153238042269),
+        name: "hangTime",
+        info: `
+        <h3>Hangtime</h3>
+        <p>The best trampoline park around!<br>
+        Hangtime also has an amazing wooden indoor play area!<br>
+        Opening Hours: 10:00 - 17:00<br>
+        Price: £8/person</p>`
+      },
+      {
+        position: new google.maps.LatLng(51.23001282640668, 2.9619333025990624),
+        name: "spuikom",
+        info: `
+        <h3>VVW Inside</h3>
+        <p>From sailing to building aquadrones.<br>
+        Guaranteed fun within biking distance!<br>
+        Opening Hours: 09:00 - 19:00<br>
+        Price: £15/person</p>`
+      },
+      {
+        position: new google.maps.LatLng(51.303612498470386, 3.08606321389479),
+        name: "surfingelephant",
+        info: `
+        <h3>Surfing Elephant</h3>
+        <p>The coolest surfclub around.<br>
+        4 watersports in a day.<br>
+        Opening Hours: 09:00 - 19:00<br>
+        Price: £20/person</p>`
+      },
+      {
+        position: new google.maps.LatLng(51.28504266161536, 3.0612400675016076),
+        name: "centerparcs",
+        info: `
+        <h3>Center Parcs</h3>
+        <p>Aquamundo<br>
+        Tropical indoor pool for all ages.<br>
+        Opening Hours: 10:00 - 18:00<br>
+        Price: £8/person</p>`
+      },
+      
+    ];
+
+    // loop over coordinates and add them onto map
+    // show info when marker is clicked
+
+    for (let i = 0; i < activities.length; i++) {
+      const marker = new google.maps.Marker({
+        position: activities[i].position,
+        map: map,
+      });
+      marker.addListener("click", function() {
+        document.getElementById("activityInfo").innerHTML = activities[i].info;
+      })
+    }
+
+    // add marker for Horizon
 
     const horizonMarker = new google.maps.Marker({
       position: horizon,
       map: map,
     });
 
-    const seaLifeMarker = new google.maps.Marker({
-        position: seaLife,
-        map: map,
-    });
-
-    const horizonEducatiefMarker = new google.maps.Marker({
-        position: horizonEducatief,
-        map: map,
-    });
-
-    const hangTimeMarker = new google.maps.Marker({
-        position: hangTime,
-        map: map,
-    });
+    // Show alert when Horizon is clicked 
 
     horizonMarker.addListener("click", () => {
     alert("You are staying here!");
-    });
-
-    seaLifeMarker.addListener("click", () => {
-      document.getElementById("activityInfo").innerHTML = `
-      <h3>Sea Life</h3>
-      <p>Discover the magic of the underwater world.
-      Come face to face with wildlife and learn all about how we can help ocean conservation.<br>
-      Opening Hours: 10:00 - 17:00<br>
-      Price: €15/person</p>`
-    });
-
-    horizonEducatiefMarker.addListener("click", () => {
-      document.getElementById("activityInfo").innerHTML = `
-      <h3>Horizon Educatief</h3>
-      <p>Choose from a wide range of educational and artistic guided activities.<br>
-      From exploring the dunes, learning all about plankton to meeting Pirate Pete.<br>
-      Opening Hours: 10:00 - 17:00<br>
-      Price: Depends on activity.</p>`
-    });
-
-    hangTimeMarker.addListener("click", () => {
-      document.getElementById("activityInfo").innerHTML = `
-      <h3>Hangtime</h3>
-      <p>The best trampoline park around!<br>
-      Hangtime also has an amazing wooden indoor play area!<br>
-      Opening Hours: 10:00 - 17:00<br>
-      Price: £8/person</p>`
     });
 
   }
@@ -82,10 +123,3 @@ $(document).ready(function() {
       });
     });
 });
-
-
-
-
-
-
-
